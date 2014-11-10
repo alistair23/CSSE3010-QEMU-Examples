044a40eb610b607aa468b241489140f0233b548c
Alistair Francis <alistair23@gmail.com>
2014-11-10 12:27:09 +1000

    demo3: Update spacing and demonstration information
    Signed-off-by: Alistair Francis <alistair23@gmail.com>

--- Demo/demo3_gpio_interrupt/main.c
+++ Demo/demo3_gpio_interrupt/main.c
@@ -18,29 +18,33 @@ void main(void) {
 
 void Hardware_init() {
 
-  debug_printf("Initialising\n");
+    debug_printf("Initialising\n");
 
-	GPIO_InitTypeDef  GPIO_InitStructure;
-  	NVIC_InitTypeDef   NVIC_InitStructure;
-	EXTI_InitTypeDef   EXTI_InitStructure;
+    GPIO_InitTypeDef  GPIO_InitStructure;
+    NVIC_InitTypeDef   NVIC_InitStructure;
+    EXTI_InitTypeDef   EXTI_InitStructure;
 
-	NP2_LEDInit();		//Initialise Blue LED
-	NP2_LEDOff();		//Turn off Blue LED
+    NP2_LEDInit();   //Initialise Blue LED
+    NP2_LEDOff();    //Turn off Blue LED
 
-  	/* Enable DO clock */
-  	RCC_AHB1PeriphClockCmd(NP2_D0_GPIO_CLK, ENABLE);
+    /* Enable DO clock */
+    RCC_AHB1PeriphClockCmd(NP2_D0_GPIO_CLK, ENABLE);
 
-  	/* Enable SYSCFG clock for interrupt hardware */
-  	RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);
-  
-  	/* Configure D0 pin as pull down input */
-  	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN;
-  	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_DOWN;
-  	GPIO_InitStructure.GPIO_Pin = NP2_D0_PIN;
-  	GPIO_Init(NP2_D0_GPIO_PORT, &GPIO_InitStructure);
+    /* Enable SYSCFG clock for interrupt hardware */
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);
 
-  	/* Connect external interrupt EXTI Line0 to DO pin */
-  	SYSCFG_EXTILineConfig(NP2_D0_EXTI_PORT, NP2_D0_EXTI_SOURCE);
+    /* Configure D0 pin as pull down input */
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN;
+    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_DOWN;
+    GPIO_InitStructure.GPIO_Pin = NP2_D0_PIN;
+    GPIO_Init(NP2_D0_GPIO_PORT, &GPIO_InitStructure);
+
+    /* Connect external interrupt EXTI Line0 to DO pin */
+    /* Original - D0 - GPIO S c 16 128 */
+    /* New      - D0 - GPIO S c 16 64 */
+    /* New      - D0 - GPIO S a 16 8 */
+    SYSCFG_DeInit();
+    SYSCFG_EXTILineConfig(NP2_D0_EXTI_PORT, NP2_D0_EXTI_SOURCE);
 
   	/* Configure EXTI Line0 for rising edge detection */
   	EXTI_InitStructure.EXTI_Line = NP2_D0_EXTI_LINE;
