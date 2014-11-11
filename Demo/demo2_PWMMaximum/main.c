d306ff4e702ccdfdec02409192568784a9c07d90
Alistair Francis <alistair23@gmail.com>
2014-11-11 16:36:21 +1000

    demo2: Loop the main function
    Also remove un-used code and reduce the delay
    
    Signed-off-by: Alistair Francis <alistair23@gmail.com>

--- Demo/demo2_PWMMaximum/main.c
+++ Demo/demo2_PWMMaximum/main.c
@@ -9,27 +9,24 @@ void Delay(__IO unsigned long nCount);
 
 
 void main(void)    {
-    char output[11] = "Hello World";
-    int numbers[] = {1, 2, 3, 4};
-    int *numbers_point = numbers;
-    int i;
-
     NP2_boardinit();
     Hardware_init();
     Servo_Pan_Init();
     Servo_Tilt_Init();
 
-    debug_printf("Starting...\n");
+    while (1) {
+        debug_printf("Starting...\n");
 
-    debug_printf("Setting Correct Values\n");
-    Servo_Pan_Set_Angle(40);
-    Servo_Tilt_Set_Angle(60);
+        debug_printf("Setting Correct Values\n");
+        Servo_Pan_Set_Angle(40);
+        Servo_Tilt_Set_Angle(60);
 
-    Delay(0x30000000);
+        Delay(0x300000);
 
-    debug_printf("Setting Incorrect Values\n");
-    Servo_Pan_Set_Angle(-90);
-    Servo_Tilt_Set_Angle(100);
+        debug_printf("Setting Incorrect Values\n");
+        Servo_Pan_Set_Angle(-90);
+        Servo_Tilt_Set_Angle(100);
+    }
 }
 
 void Hardware_init(void) {
