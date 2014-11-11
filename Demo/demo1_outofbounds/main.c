ffc6c96dd5d1f93c390bac737810db46f59f65df
Alistair Francis <alistair23@gmail.com>
2014-11-11 15:41:49 +1000

    demo1: Loop over the demo
    Signed-off-by: Alistair Francis <alistair23@gmail.com>

--- Demo/demo1_outofbounds/main.c
+++ Demo/demo1_outofbounds/main.c
@@ -16,30 +16,41 @@ void main(void)    {
     NP2_boardinit();
     Hardware_init();
 
-    /* Out of bounds access - example 1 */
-    debug_printf("Example 1\n\n");
-    for (i = 0; i < 12; i++) {
-        debug_printf("i: %d, val: %c\n", i, output[i]);
+    while (1) {
+        /* Out of bounds access - example 1 */
+        debug_printf("\nExample 1\n");
+        for (i = 0; i < 12; i++) {
+            debug_printf("i: %d, val: %c\n", i, output[i]);
+            NP2_LEDToggle();
+        }
+
+        /* Out of bounds access - example 2 */
+        debug_printf("\nExample 2\n");
+        for (i = 0; i < 5; i++) {
+            debug_printf("i: %d, val: %d\n", i, numbers_point[i]);
+            NP2_LEDToggle();
+        }
+
+        numbers_point = NULL;
+        i = 0;
+
+        /* Out of bounds access - example 3 */
+        debug_printf("\nExample 3\n");
+        debug_printf("val: %d\n", i, numbers_point);
+
         NP2_LEDToggle();
+        Delay(0x7FFF00);    //Delay 1s.
+        numbers_point = numbers;
     }
-
-    /* Out of bounds access - example 2 */
-    debug_printf("Example 2\n\n");
-    for (i = 0; i < 5; i++) {
-        debug_printf("i: %d, val: %d\n", i, numbers_point[i]);
-        NP2_LEDToggle();
-    }
-
-    numbers_point = NULL;
-    i = 0;
-
-    /* Out of bounds access - example 3 */
-    debug_printf("Example 3\n\n");
-    debug_printf("val: %d\n", i, numbers_point);
-    NP2_LEDToggle();
 }
 
 void Hardware_init(void) {
     NP2_LEDInit();
     NP2_LEDOff();
+}
+
+void Delay(__IO unsigned long nCount) {
+
+    while(nCount--) {
+    }
 }
